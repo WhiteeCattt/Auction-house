d04999e3ce39df70c93c293ed862828e4d759442
Белый кот <118972678+WhiteeCattt@users.noreply.github.com>
2024-01-10 19:52:46 +0200

    Create manifest.js

---
+++ RP/manifest.js
@@ -0,0 +1,17 @@
+{
+	"format_version": 2,
+	"header": {
+		"name": "Auction house RP",
+		"description": "",
+		"uuid": "d00a6f86-6cb6-47d6-b058-3acbd33a5540",
+		"version": [ 1, 0, 0 ],
+		"min_engine_version": [ 1, 20, 40 ]
+	},
+	"modules": [
+		{
+			"type": "resources",
+			"uuid": "76ebcd1b-dd99-402f-b868-345a04fcb556",
+			"version": [ 1, 0, 0 ]
+		}
+	]
+}
