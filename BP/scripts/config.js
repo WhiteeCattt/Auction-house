af9a074f386765203d551591b0a95924d68a3e73
Белый кот <118972678+WhiteeCattt@users.noreply.github.com>
2024-01-10 19:47:14 +0200

    Create config.js

---
+++ BP/scripts/config.js
@@ -0,0 +1,10 @@
+export default {
+    prefix: "§7[§l§bAuction§r§7] §r", // Prefix
+    cmdPrefix: "-", // Command prefix
+    adminTag: "Admin", // Admin tag
+    maxPlayerAuctions: 3, // Maximum number of auctions for a player
+    maxAuctions: 99999, // Maximum number of auctions
+    expires: 604800000, // Expiration time (In milliseconds)
+    maxPrice: 100000000, // Maximum price
+    moneyObject: "money" // Scorboard money object
+}
