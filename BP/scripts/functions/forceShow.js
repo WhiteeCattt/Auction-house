5707d04ff07a15c999fa328d28fd594650594507
Белый кот <118972678+WhiteeCattt@users.noreply.github.com>
2024-01-10 19:48:54 +0200

    Create forceShow.js

---
+++ BP/scripts/functions/forceShow.js
@@ -0,0 +1,13 @@
+import { system } from "@minecraft/server";
+import { FormCancelationReason } from "@minecraft/server-ui";
+
+export async function forceShow(player, form, timeout = Infinity) {
+    const startTick = system.currentTick;
+    while ((system.currentTick - startTick) < timeout) {
+        const response = await (form.show(player));
+        if (response.cancelationReason !== FormCancelationReason.UserBusy) {
+            return response;
+        }
+    };
+    throw new Error(`Timed out after ${timeout} ticks`);
+}
