3b97e15b3b6b25304638d26cdddb722c2347076e
Белый кот <118972678+WhiteeCattt@users.noreply.github.com>
2024-01-22 21:05:02 +0200

    Update config.js

--- BP/scripts/config.js
+++ BP/scripts/config.js
@@ -1,10 +1,14 @@
 export default {
-    prefix: "§7[§l§bAuction§r§7] §r", // Prefix
-    cmdPrefix: "-", // Command prefix
-    adminTag: "Admin", // Admin tag
-    maxPlayerAuctions: 3, // Maximum number of auctions for a player
-    maxAuctions: 99999, // Maximum number of auctions
-    expires: 604800000, // Expiration time (In milliseconds)
-    maxPrice: 100000000, // Maximum price
-    moneyObject: "money" // Scorboard money object
+    prefix: "§7[§l§bAuction house§r§7] §r", // Prefix
+    cmd_prefix: "-", // Command prefix
+    admin_tag: "Admin", // Admin tag
+    max_player_auctions: 3, // Maximum number of auctions for a player
+    max_auctions: 10000, // Maximum number of auctions
+    expires: 604800000, // Expiration time (In milliseconds). Infinity = never
+    max_price: 100000000, // Maximum price
+    money_object: "money", // Scorboard money object
+    banned_items: [ // Banned items
+        "minecraft:bedrock",
+        "minecraft:deny"
+    ]
 }
